997eb06d93fce1359d0b4bc0ff741b038d99fa6b
Mateusz Kalinowski <mateusz.kalinowski@icloud.com>
2018-11-12 17:07:35 +0100

    Initial commit

---
+++ src/main/java/pl/dormitorymaintenancesystem/DormitorymaintenancesystemApplication.java
@@ -0,0 +1,12 @@
+package pl.dormitorymaintenancesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormitorymaintenancesystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DormitorymaintenancesystemApplication.class, args);
+    }
+}
